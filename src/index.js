1bae0b899162628d49f1c9924ce3d20604547dc0
Shivam Sandilya <shivamroy1092004@gmail.com>
2026-08-23 17:35:54 +0530

    utils understanding

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import DB_connect from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({path:".env"})
 
 DB_connect()
+
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed!!",err)
+})
 
 // import mongoose from "mongoose";
 // import { DB_NAME} from "./constants";
